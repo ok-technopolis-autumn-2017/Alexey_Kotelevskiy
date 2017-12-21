53487e8ac085561c0dd3421d28e2d1115f65efad
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2017-12-21 16:31:39 +0300

    textarea autosize

--- src/scripts/components/TodoItem.js
+++ src/scripts/components/TodoItem.js
@@ -7,7 +7,8 @@ var templatesEngine = require('../modules/templatesEngine');
 
 var READY_MODIFICATOR = '__ready';
 var HIDDEN_MODIFICATOR = '__hide';
-
+var HIDDEN_DIV_CLASS = 'hidden-container';
+var MIN_TEXT_HEIGHT  = '40';
 /**
  * @param itemData
  * @implements {EventListener}
@@ -35,9 +36,19 @@ function TodoItemConstructor(itemData) {
         this._setReadyModificator(true);
     }
 
+    var hiddenDiv = document.getElementsByClassName(HIDDEN_DIV_CLASS)[0];
+    if (hiddenDiv == null) {
+        hiddenDiv = document.createElement('div');
+        hiddenDiv.classList.add(HIDDEN_DIV_CLASS);
+        hiddenDiv.style.width = getComputedStyle(this._text).width;
+        document.getElementsByTagName('body')[0].appendChild(hiddenDiv);
+    }
+    this._hiddenBlock = hiddenDiv;
     this._markReady.addEventListener('change', this);
     this._removeAction.addEventListener('click', this);
     this._text.addEventListener('input', this);
+
+
 }
 
 extendConstructor(TodoItemConstructor, Eventable);
@@ -67,10 +78,34 @@ todoItemConstructorPrototype.handleEvent = function (e) {
             }
             break;
         case 'input':
-            this.setText(this._text.innerText);
+            this.resizeText(this._text.value);
+            this.setText(this._text.value);
             break;
+
     }
 };
+
+
+todoItemConstructorPrototype.resizeText = function (text) {
+
+    var content = text;
+    var textHeight;
+    var textFontSize =  parseInt(window.getComputedStyle(this._hiddenBlock).fontSize, 10)
+    content = content.replace(/\n/g, '<br>');
+    this._hiddenBlock.innerHTML = content;
+    textHeight = parseInt(window.getComputedStyle(this._hiddenBlock).height, 10);
+
+    if (textHeight > MIN_TEXT_HEIGHT)
+    {
+        this._text.style.height = textHeight + textFontSize + "px";
+    }
+    else
+    {
+        this._text.style.height = MIN_TEXT_HEIGHT + "px";
+    }
+    return this;
+}
+
 
 /**
  * @param {String} text
